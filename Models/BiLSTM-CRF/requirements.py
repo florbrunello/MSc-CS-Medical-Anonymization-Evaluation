2f8d406bddf5d4e27528177f4614cc3e7953a0cd
Florencia Brunello <florenciabrunello@mi.unc.edu.ar>
2026-01-16 12:05:31 -0300

    chore(models): rename folder

--- Models/BiLSTM-CRF/requirements.py
+++
@@ -1,8 +0,0 @@
-import nltk
-
-# Descarga los recursos necesarios para el correcto funcionamiento de la librería nltk
-nltk.download('cess_esp')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('averaged_perceptron_tagger_eng')
-nltk.download('punkt')
-nltk.download('punkt_tab')
